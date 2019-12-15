62198872e33dddb46b64f959a426991f19b0ffcf
yumoqing <yumoqing@gmail.com>
2019-12-15 16:19:09 +0800

    bugfix

---
+++ setup_windows.cmd
@@ -0,0 +1,12 @@
+python -m pip install --upgrade pip 
+pip install wheel setuptools 
+pip install docutils pygments pypiwin32 kivy_deps.sdl2 kivy_deps.glew
+pip install pillow
+pip install ffpyplayer
+pip install kivy
+pip install git+https://github.com/yumoqing/appPublic
+pip install git+https://github.com/yumoqing/sqlor
+pip install git+https://github.com/yumoqing/kivycalendar
+git install git+https://github.com/yumoqing/kivyblocks
+
+
