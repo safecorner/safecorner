17548ecdbd10369f23241d68a16f2f2810de21ff
yumoqing <yumoqing@gmail.com>
2020-03-15 10:41:24 +0800

    'bugfix'

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import os
 os.environ["KIVY_VIDEO"] = "ffpyplayer"
 import sys
+from kivy.app import App
+from kivy.clock import Clock
 from kivy.core.window import Window
+from kivy.logger import Logger
 from appPublic.folderUtils import ProgramPath
 from appPublic.jsonConfig import getConfig
-from kivyblocks.blocksapp import BlocksApp
+from kivyblocks.blocksapp import appBlocksHack
 from kivyblocks.blocks import registerWidget
+from kivyblocks.pagescontainer import PageContainer
 
 from channelbox import ChannelBox
 from livetv import LiveTV
@@ -13,6 +17,28 @@ from songviewer import SongViewer
 from picviewer import PicViewer
 from movieviewer import MovieViewer
 
+class MyApp(App):
+	def build(self):
+		x = PageContainer()
+		Clock.schedule_once(self.build1,0)
+		return x
+
+	def build1(self,t=None):
+		x = None
+		config = getConfig()
+		config.uihome = config.uihome_local
+		x = self.blocks.widgetBuild(config.root)
+		if x is None:
+			# print('*****************Use Internet IP***********************')
+			# config.uihome = config.uihome_internet
+			# print('*****************Use Internet IP***********************')
+			# x = self.blocks.widgetBuild(config.root)
+			# if x is None:
+			#	alert(str(self.config.root)+': cannt build widget')
+			return
+		self.root.add_widget(x)
+		return 
+	
 if __name__ == '__main__':
 	pp = ProgramPath()
 	workdir = pp
@@ -25,6 +51,7 @@ if __name__ == '__main__':
 	registerWidget('MovieViewer',MovieViewer)
 	registerWidget('PicViewer',PicViewer)
 	registerWidget('ChannelBox', ChannelBox)
-	myapp = BlocksApp()
+	myapp = MyApp()
+	appBlocksHack(myapp)
 	myapp.run()
 
