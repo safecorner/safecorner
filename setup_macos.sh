7ff976ed42b8249b5fba0d5d71cf6881d8d87965
safecorner <safecorner@163.com>
2020-03-22 17:33:44 +0800

    macos package

---
+++ setup_macos.sh
@@ -0,0 +1,13 @@
+python -m pip install --upgrade pip 
+pip install wheel setuptools 
+# pip install docutils pygments pypiwin32 kivy_deps.sdl2 kivy_deps.glew
+# brew install pkg-config sdl2 sdl2_image sdl2_ttf sdl2_mixer gstreamer
+pip install cryptography
+pip install pillow
+pip install ffpyplayer
+pip install opencv-python
+pip install kivy
+pip install git+https://github.com/yumoqing/appPublic
+pip install git+https://github.com/yumoqing/sqlor
+pip install git+https://github.com/yumoqing/kivycalendar
+pip install git+https://github.com/yumoqing/kivy-blocks
